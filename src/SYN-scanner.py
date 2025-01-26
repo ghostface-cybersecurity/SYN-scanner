2240afa5c5d78bd27d017851c4fa8176295a407d
ghostface <wseven347.devopser@gmail.com>
2025-01-26 03:15:44 -0700

    Create SYN-scanner.py

---
+++ src/SYN-scanner.py
@@ -0,0 +1,33 @@
+  #####   ##  ##   ##   ##            #####     ####     ##     ##   ##  ##   ##  #######  ######
+ ##   ##  ##  ##   ###  ##           ##   ##   ##  ##   ####    ###  ##  ###  ##   ##   #   ##  ##
+ #        ##  ##   #### ##           #        ##       ##  ##   #### ##  #### ##   ## #     ##  ##
+  #####    ####    ## ####  ######    #####   ##       ##  ##   ## ####  ## ####   ####     #####
+      ##    ##     ##  ###                ##  ##       ######   ##  ###  ##  ###   ## #     ## ##
+ ##   ##    ##     ##   ##           ##   ##   ##  ##  ##  ##   ##   ##  ##   ##   ##   #   ##  ##
+  #####    ####    ##   ##            #####     ####   ##  ##   ##   ##  ##   ##  #######  #### ##
+
+__________________________________________________________________________________________________
+                                             ABOUT
+__________________________________________________________________________________________________
+Description: SYN-scan tool in python
+Program: SYN-scanner
+Language: Python 3.12.7
+Tested on: Linux 6.11.2
+Author: ghostface-cybersecurity
+
+_________________________________________________________________________________________________
+                                         PROGRAM LAUNCH
+_________________________________________________________________________________________________
+sudo python3 SYN-scanner.py <IP> <PORT>
+sudo python3 SYN-scanner.py 192.168.1.101 8000
+
+_________________________________________________________________________________________________
+                                        LEGAL STATEMENT
+_________________________________________________________________________________________________
+By downloading, modifying, redistributing, and/or executing SYN-scanner, the
+user agrees to the contained LEGAL.txt statement found in this repository.
+
+I, ghostface-cybersecurity, the creator, take no legal responsibility for unlawful actions
+caused/stemming from this program. 
+
+Use responsibly and ethically!
